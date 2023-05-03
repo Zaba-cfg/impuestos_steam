86d093ee81852e9528a09c2dd54b0422f07ded33
Agustin <agu-zaballa@hotmail.com>
2023-05-02 23:00:00 -0300

    Arreglos en el grid template

---
+++ script.js
@@ -0,0 +1,22 @@
+let calculo = 0
+let inicio = Number(0)
+
+let inputCalculo = document.getElementById('inputCalculadora')
+
+inputCalculo.oninput = () => {
+    let calculo = inputCalculo.value * 1.75
+    if(Number.isNaN(calculo)) {
+        calculo = 0
+    }
+    renderizarCalculo(calculo)
+}
+
+function renderizarCalculo(calculo) {
+    contenedorCalculos.innerHTML = ''
+    let mostrarCalculo = document.createElement('div')
+    mostrarCalculo.className = 'producto'
+    mostrarCalculo.innerHTML = `
+<h2 id= "resultadoCalculadora">${calculo} ARS$</h2>
+`
+    contenedorCalculos.append(mostrarCalculo)
+}
