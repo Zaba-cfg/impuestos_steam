7c2e89d5e3b86d30c9421bfc1c8c801160dd88b7
Zaba <agu-zaballa@hotmail.com>
2023-06-20 20:19:33 -0300

    Texto de impuestos

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 let calculo = 0
 let inicio = Number(0)
 
-let inputCalculo = document.getElementById('inputCalculadora')
+let inputCalculo = document.getElementById("inputCalculadora")
 
 inputCalculo.oninput = () => {
-    let calculo = inputCalculo.value * 1.75
-    if(Number.isNaN(calculo)) {
-        calculo = 0
-    }
-    renderizarCalculo(calculo)
+   let calculo = inputCalculo.value * 1.75
+   if (Number.isNaN(calculo)) {
+      calculo = 0
+   }
+   renderizarCalculo(calculo)
 }
 
 function renderizarCalculo(calculo) {
-    contenedorCalculos.innerHTML = ''
-    let mostrarCalculo = document.createElement('div')
-    mostrarCalculo.className = 'producto'
-    mostrarCalculo.innerHTML = `
+   contenedorCalculos.innerHTML = ""
+   let mostrarCalculo = document.createElement("div")
+   mostrarCalculo.className = "producto"
+   mostrarCalculo.innerHTML = `
 <h2 id= "resultadoCalculadora">${calculo} ARS$</h2>
+<p>Impuestos calculados al 75% (actual)</p>
 `
-    contenedorCalculos.append(mostrarCalculo)
+   contenedorCalculos.append(mostrarCalculo)
 }
