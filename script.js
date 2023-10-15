86e1c3bd26a97b513675ff0051c600fb90de2de0
Zaba-cfg <111442041+Zaba-cfg@users.noreply.github.com>
2023-10-15 20:21:35 -0300

    Actualización de precios 75% a 100%

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let inicio = Number(0)
 let inputCalculo = document.getElementById("inputCalculadora")
 
 inputCalculo.oninput = () => {
-   let calculo = inputCalculo.value * 1.75
+   let calculo = inputCalculo.value * 2
    if (Number.isNaN(calculo)) {
       calculo = 0
    }
@@ -17,7 +17,8 @@ function renderizarCalculo(calculo) {
    mostrarCalculo.className = "producto"
    mostrarCalculo.innerHTML = `
 <h2 id="resultadoCalculadora">${calculo} ARS$</h2>
-<p id="informacion">Impuestos calculados al 75% (actual)</p>
+<p id="informacion">Impuestos calculados al 100% (actual)</p>
+<p id="informacion2">El doble papi no es tan difícil</p>
 `
    contenedorCalculos.append(mostrarCalculo)
 }
