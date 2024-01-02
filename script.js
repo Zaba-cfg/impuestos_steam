ee20460319b0541263c0e878f4d2a1f4a9364f07
Zaba-cfg <111442041+Zaba-cfg@users.noreply.github.com>
2024-01-02 20:50:25 -0300

    Actualización a dólares

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let inicio = Number(0)
 let inputCalculo = document.getElementById("inputCalculadora")
 
 inputCalculo.oninput = () => {
-   let calculo = inputCalculo.value * 2
+   let calculo = inputCalculo.value * 1328.4
    if (Number.isNaN(calculo)) {
       calculo = 0
    }
@@ -17,8 +17,8 @@ function renderizarCalculo(calculo) {
    mostrarCalculo.className = "producto"
    mostrarCalculo.innerHTML = `
 <h2 id="resultadoCalculadora">${calculo} ARS$</h2>
-<p id="informacion">Impuestos calculados al 100% (actual)</p>
-<p id="informacion2">El doble papi no es tan difícil</p>
+<p id="informacion">Impuestos calculados al Dolar tarjeta</p>
+<p id="informacion2">$1328.40 (2/1/24)</p>
 `
    contenedorCalculos.append(mostrarCalculo)
 }
